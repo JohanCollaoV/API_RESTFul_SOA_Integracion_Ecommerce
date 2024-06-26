4693c512312f4a0bf601408de2df937d2be84ef1
Johan Collao Vivanco <collao.johan@outlook.com>
2024-06-26 17:13:13 -0400

    Primera pruebas,modulo inventario

--- ecommerce/settings_test.py
+++ ecommerce/settings_test.py
@@ -3,6 +3,6 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
